8195791cbc6674efcd837f2b650b267345032578
Amit Raccah <amitraccah1990@gmail.com>
2025-06-03 21:12:48 +0300

    FirstPush

---
+++ Assets/Scripts/InputModule/Crouch/CrouchAnimationListener.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class CrouchAnimationListener : MonoBehaviour
+{
+    [SerializeField] private AnimationDriver animationDriver;
+
+    private void OnEnable()
+    {
+        Debug.Log("Crouch Listener Enabled");
+        EventBus.Subscribe<PlayerCrouchEvent>(OnCrouchEvent);
+        EventBus.Subscribe<PlayerUncrouchEvent>(OnUncrouchEvent);
+    }
+
+    private void OnDisable()
+    {
+        EventBus.Unsubscribe<PlayerCrouchEvent>(OnCrouchEvent);
+        EventBus.Unsubscribe<PlayerUncrouchEvent>(OnUncrouchEvent);
+    }
+
+    private void OnCrouchEvent(PlayerCrouchEvent evt)
+    {
+        animationDriver.SetBool("IsCrouching", true);
+    }
+
+    private void OnUncrouchEvent(PlayerUncrouchEvent e)
+    {
+        Debug.Log("► Un-crouch event received");
+        animationDriver.SetBool("IsCrouching", false);
+    }
+
+
+
+}
