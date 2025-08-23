e7c9014afaeeb1c03f7fcbd6e88856cde78f7c23
Amit Raccah <amitraccah1990@gmail.com>
2025-08-23 12:50:27 +0300

    InputBufferChange+ScriptableObjects Corrections
    Took off unwanted scripts from enemy, added heavy punch again to scriptable object, moved instance in input buffer from OnEnable to Awake

--- Assets/Scripts/InputModule/InputBuffer.cs
+++ Assets/Scripts/InputModule/InputBuffer.cs
@@ -1,85 +1,68 @@
-﻿using UnityEngine;
-using System.Collections.Generic;
-
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class InputBuffer : MonoBehaviour
 {
-    [SerializeField] private int bufferSize = 12;
+    private int bufferSize = 12;
 
-    public static InputBuffer Instance;
+    private bool log = false;
 
-    private List<FrameInput> buffer = new List<FrameInput>();
+    public static InputBuffer Instance { get; private set; }
 
-    private int currentFrame;
+    private readonly List<FrameInput> buffer = new();
 
     private void Awake()
     {
-   //     Instance = this;
-    }
-
-    private void OnEnable()
-    {
+        if (Instance != null && Instance != this)
+        {
+            Debug.LogWarning("[InputBuffer] Duplicate found, disabling this one.", this);
+            enabled = false;
+            return;
+        }
         Instance = this;
     }
 
     private void OnDisable()
     {
-        if (Instance == this)
-        {
-            Instance = null;
-        }
+        if (Instance == this) Instance = null;
     }
+
     public void Add(InputType input)
     {
-        if (bufferSize == 0)                    
-        {
-            return;                            
-        }
+        if (bufferSize == 0) return;
 
+        int frame = Time.frameCount;
         if (buffer.Count > 0)
         {
             FrameInput last = buffer[buffer.Count - 1];
-            if (last.inputType == input && last.frame == currentFrame)
-            {
-                return;                         
-            }
+            if (last.inputType == input && last.frame == frame)
+                return; 
         }
 
-        buffer.Add(new FrameInput(input, currentFrame));
+        buffer.Add(new FrameInput(input, frame));
     }
 
+    public List<FrameInput> GetBuffer() => buffer;
 
-    public List<FrameInput> GetBuffer()
-    {
-        return buffer;
-    }
+    public void Clear() => buffer.Clear();
 
-
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     private void Update()
     {
-        currentFrame++;
-        Debug.Log($"BUF count={buffer.Count}");
-
-
         if (bufferSize == 0)
         {
-            buffer.Clear();
+            if (buffer.Count > 0) buffer.Clear();
             return;
         }
 
+        int now = Time.frameCount;
         for (int i = buffer.Count - 1; i >= 0; i--)
         {
-            int age = currentFrame - buffer[i].frame;
-            if (age >= bufferSize)        
-            {
+            int age = now - buffer[i].frame;
+            if (age >= bufferSize)
                 buffer.RemoveAt(i);
-            }
         }
+
+        if (log && (now % 30 == 0)) // 0.5 sec
+            Debug.Log($"[InputBuffer] count={buffer.Count}");
     }
 }
