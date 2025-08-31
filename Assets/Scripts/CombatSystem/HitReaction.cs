2b0cd8646f141e093fcaaec919016b867f6996b3
Amit Raccah <amitraccah1990@gmail.com>
2025-08-31 22:01:06 +0300

    HitReaction
    Can move from animator tag "Attack" to "Hit" during animation

--- Assets/Scripts/CombatSystem/HitReaction.cs
+++ Assets/Scripts/CombatSystem/HitReaction.cs
@@ -1,92 +1,65 @@
 ﻿using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
-
 public class HitReaction : MonoBehaviour
-
 {
-
-    [Header("Names")]
-
     [SerializeField] private string hitTrigger = "Hit";
-
     [SerializeField] private string attackTag = "Attack";
 
-    Animator anim;
+    private Animator anim;
+    private int myId;
+    private int hitHash;
+    private bool queuedHit;
 
-    int myId, hitHash;
-
-    bool queuedHit = false;          // ← דגל
-
-    void Awake()
-
+    private void Awake()
     {
-
         anim = GetComponent<Animator>();
-
         myId = gameObject.GetInstanceID();
-
         hitHash = Animator.StringToHash(hitTrigger);
-
     }
 
-    void OnEnable() => CombatBus.Subscribe<DamageEvent>(OnDamage);
+    private void OnEnable() => CombatBus.Subscribe<DamageEvent>(OnDamage);
+    private void OnDisable() => CombatBus.Unsubscribe<DamageEvent>(OnDamage);
 
-    void OnDisable() => CombatBus.Unsubscribe<DamageEvent>(OnDamage);
-
-    void OnDamage(DamageEvent e)
-
+    private void OnDamage(DamageEvent e)
     {
-
         if (e.targetId != myId) return;
 
         var bc = GetComponent<BlockController>();
-
         if (bc && bc.IsBlocking && IsFacingAttacker(e.attackerId)) return;
 
-        if (anim.GetCurrentAnimatorStateInfo(0).IsTag(attackTag) ||
-
-            (anim.IsInTransition(0) &&
-
-             anim.GetNextAnimatorStateInfo(0).IsTag(attackTag)))
-
-        {
-
-            queuedHit = true;
-
-            return;
-
-        }
-
+        anim.ResetTrigger(hitHash);
         anim.SetTrigger(hitHash);
 
+        if (IsInAttack()) queuedHit = true;
     }
 
     public void CheckQueuedHit()
-
     {
-
         if (!queuedHit) return;
-
         queuedHit = false;
-
+        anim.ResetTrigger(hitHash);
         anim.SetTrigger(hitHash);
-
     }
 
-    bool IsFacingAttacker(int attackerId)
-
+    private bool IsInAttack()
     {
+        var s = anim.GetCurrentAnimatorStateInfo(0);
+        if (s.IsTag(attackTag)) return true;
+        if (anim.IsInTransition(0))
+        {
+            var n = anim.GetNextAnimatorStateInfo(0);
+            if (n.IsTag(attackTag)) return true;
+        }
+        return false;
+    }
 
+    private bool IsFacingAttacker(int attackerId)
+    {
         if (!AttackActivator.TransformsById.TryGetValue(attackerId, out var atk))
-
             return false;
 
         Vector3 dir = (atk.position - transform.position).normalized;
-
         return Vector3.Dot(transform.forward, dir) > 0.3f;
-
     }
-
 }
-
