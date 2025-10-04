5b477e5096203b58b268847e26ece15953b58a6a
Amit Raccah <amitraccah1990@gmail.com>
2025-10-05 01:39:24 +0300

    Hazards

---
+++ Assets/Scripts/HazardContactHit.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class HazardContactHit : MonoBehaviour
+{
+    [SerializeField] private AttackData attack;
+    [SerializeField] private LayerMask targetLayers;
+
+    private int attackerId;
+    private Collider col;
+
+    void Awake()
+    {
+        col = GetComponent<Collider>();
+        col.isTrigger = true;
+    }
+
+    void OnEnable()
+    {
+        attackerId = gameObject.GetInstanceID();
+        AttackActivator.TransformsById[attackerId] = transform;
+    }
+
+    void OnDisable()
+    {
+        AttackActivator.TransformsById.Remove(attackerId);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!attack) return;
+        if (((1 << other.gameObject.layer) & targetLayers.value) == 0) return;
+
+        Transform root = other.attachedRigidbody ? other.attachedRigidbody.transform : other.transform.root;
+        if (!root || root == transform.root) return;
+
+        HitResolve.PublishDamageAndFx(attackerId, attack, other, transform.position, transform);
+    }
+}
