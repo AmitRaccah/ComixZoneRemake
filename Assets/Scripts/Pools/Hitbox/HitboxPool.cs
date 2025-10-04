91d92371748968a08cdb37f09edcba41159f6492
Amit Raccah <amitraccah1990@gmail.com>
2025-10-04 20:47:52 +0300

    improved hitbox pool

--- Assets/Scripts/Pools/Hitbox/HitboxPool.cs
+++ Assets/Scripts/Pools/Hitbox/HitboxPool.cs
@@ -5,32 +5,43 @@ public class HitboxPool : MonoBehaviour
 {
     public static HitboxPool Instance { get; private set; }
 
-    [SerializeField] private GameObject hitboxPrefab;
-    [SerializeField] private int prewarm = 16;
+    [Tooltip("Optional: drag pre-placed HitboxController instances here. If left empty, children will be auto-discovered.")]
+    [SerializeField] private List<HitboxController> preplaced = new();
 
     private readonly Stack<HitboxController> free = new();
 
     void Awake()
     {
         Instance = this;
-        for (int i = 0; i < Mathf.Max(0, prewarm); i++)
-            free.Push(CreateOne());
-    }
 
-    HitboxController CreateOne()
-    {
-        var go = Instantiate(hitboxPrefab, transform);
-        go.SetActive(false);
-        var hb = go.GetComponent<HitboxController>();
-        hb.AssignPool(this);
-        return hb;
+        if (preplaced == null || preplaced.Count == 0)
+        {
+            preplaced = new List<HitboxController>(GetComponentsInChildren<HitboxController>(true));
+        }
+
+        for (int i = 0; i < preplaced.Count; i++)
+        {
+            var hb = preplaced[i];
+            if (!hb) continue;
+
+            hb.AssignPool(this);
+            hb.gameObject.SetActive(false);
+            hb.transform.SetParent(transform, false);
+            free.Push(hb);
+        }
     }
 
     public HitboxController Spawn(Transform socket, AttackData data, int attackerId)
     {
-        var hb = (free.Count > 0) ? free.Pop() : CreateOne();
+        if (free.Count == 0)
+        {
+            Debug.LogWarning("HitboxPool: capacity exhausted. Add more pre-placed hitboxes to the scene.", this);
+            return null;
+        }
+
+        var hb = free.Pop();
         hb.gameObject.SetActive(true);
-        hb.Init(data, socket, attackerId);
+        hb.Init(data, socket, attackerId); 
         return hb;
     }
 
