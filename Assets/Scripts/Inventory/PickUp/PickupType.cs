e943dcb95bca344abc46e1ba5d0e8cbff2bfbbbb
Amit Raccah <amitraccah1990@gmail.com>
2025-10-20 02:53:21 +0300

    Plane

--- Assets/Scripts/Inventory/PickUp/PickupType.cs
+++ Assets/Scripts/Inventory/PickUp/PickupType.cs
@@ -3,5 +3,5 @@ public enum PickupType
 {
     Knife,
     Potion,
-    Bomb
+    Plane
 }
--- Assets/Scripts/Inventory/PotionItem.cs
+++ Assets/Scripts/Inventory/PotionItem.cs
@@ -1,14 +1,28 @@
-using System;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Potion", menuName = "Inventory/Potion")]
 public class PotionItem : Item
 {
     public int healAmount = 10;
+    public ParticleEffectData consumeEffect;
+    public AudioClip consumeClip;
 
     public override bool Use()
     {
+        Transform t = null;
+        var player = GameObject.FindGameObjectWithTag("Player");
+        if (player) t = player.transform;
+
+        if (consumeEffect != null && !string.IsNullOrEmpty(consumeEffect.vfxId) && VfxPoolManager.Instance != null && t != null)
+        {
+            Vector3 pos = ParticleEffectUtility.CalculateSpawnPosition(t.position, t, consumeEffect.localOffset);
+            VfxPoolManager.Instance.Spawn(consumeEffect.vfxId, pos, t.rotation);
+        }
+
+        if (consumeClip != null && t != null)
+            AudioSource.PlayClipAtPoint(consumeClip, t.position);
+
         CoreBus.Publish(new PotionConsumedEvent(healAmount));
-        return true; 
+        return true;
     }
 }
